56031ac85b6655fbfb2ea2f015b0e0f02d9ffc9a
AbrarShakhi <114344002+AbrarShakhi@users.noreply.github.com>
2025-10-15 23:32:14 +0600

    emni

--- java/edu/edubd/cse489_2025_3_2022_3_60_022/MainActivity.java
+++ java/edu/edubd/cse489_2025_3_2022_3_60_022/MainActivity.java
@@ -36,8 +36,8 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View v) {
                 startActivity(new Intent(
                         MainActivity.this,
-                                StudentProfileActivity.class
-                        ));
+                        StudentProfileActivity.class
+                ));
             }
         });
     }
