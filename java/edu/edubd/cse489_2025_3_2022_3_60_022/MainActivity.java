1c1c3f9625a497e335c38f1fbdadcd9d5e592361
AbrarShakhi <114344002+AbrarShakhi@users.noreply.github.com>
2025-10-15 16:18:44 +0600

    lab work done

--- java/edu/edubd/cse489_2025_3_2022_3_60_022/MainActivity.java
+++ java/edu/edubd/cse489_2025_3_2022_3_60_022/MainActivity.java
@@ -1,15 +1,18 @@
 package edu.edubd.cse489_2025_3_2022_3_60_022;
 
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
+
     private EditText etCourseCode, etDate;
     private Button btnExit, btnAddStudent;
     private ListView lvStudents;
@@ -25,23 +28,44 @@ public class MainActivity extends AppCompatActivity {
         btnAddStudent = findViewById(R.id.btnAddStudent);
         lvStudents = findViewById(R.id.lvStudents);
 
-        btnExit.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                finish();
+        btnExit.setOnClickListener(
+            new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    finish();
+                }
             }
-        });
+        );
 
-        btnAddStudent.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                startActivity(new Intent(
-                        MainActivity.this,
-                        StudentProfileActivity.class)
-                );
+        btnAddStudent.setOnClickListener(
+            new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    startActivity(
+                        new Intent(
+                            MainActivity.this,
+                            StudentProfileActivity.class
+                        )
+                    );
+                }
             }
-        });
+        );
     }
 
+    public boolean verifyCourseCode(Context c, String courseCode) {
+        if (courseCode.length() != 6 && courseCode.length() != 7) {
+            Toast.makeText(
+                    c,
+                    "Course code must be 6 or 7 char long",
+                    Toast.LENGTH_LONG
+            ).show();
+            return false;
+        }
+        return true;
+    }
 
+    public boolean verifyDate(Context c, String date) {
+        // TODO: NOT IMPLEMENTED ;
+        return true;
+    }
 }
